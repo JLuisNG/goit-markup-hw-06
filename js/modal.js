5532ec093df639b11e455e1324249f9fc27e1736
JLuisNG <127897120+JLuisNG@users.noreply.github.com>
2023-05-14 18:58:54 -0400

    s

--- js/modal.js
+++ js/modal.js
@@ -13,3 +13,22 @@
   }
 })();
 
+  const inputs = document.querySelectorAll('.modal-for-input');
+
+  inputs.forEach(input => {
+    input.addEventListener('input', () => {
+      if (input.value.length < 2) {
+        input.style.borderColor = 'red';
+        input.previousElementSibling.style.fill = 'red';
+      } else {
+        input.style.borderColor = 'green';
+        input.previousElementSibling.style.fill = 'green';
+      }
+    });
+  });
+
+
+
+
+
+
