ccb1e383bc87856c6b9bf4c3b0fc300a17dae846
JLuisNG <127897120+JLuisNG@users.noreply.github.com>
2023-05-14 16:25:16 -0400

    a

--- js/modal.js
+++ js/modal.js
@@ -1,23 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     var miBoton = document.getElementById("boton-modal");
-//     var miVentanaModal = document.getElementById("miVentanaModal");
-//     var miBotonCerrar = document.getElementsByClassName("cerrar")[0];
-  
-//     miBoton.onclick = function() {
-//       miVentanaModal.style.display = "block";
-//     }
-  
-//     miBotonCerrar.onclick = function() {
-//       miVentanaModal.style.display = "none";
-//     }
-  
-//     window.onclick = function(event) {
-//       if (event.target == miVentanaModal) {
-//         miVentanaModal.style.display = "none";
-//       }
-//     }
-//   });
-
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
